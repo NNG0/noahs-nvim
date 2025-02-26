1e904a4e7e4e3b600c1620c36256a23f08ee76d9
nng <noah.garkisch@gmail.com>
2025-02-26 16:40:13 +0100

    setup lua, python lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,9 @@ return {
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "lua_ls"
+                    "lua_ls",
+                    "pylsp",
+                    "rust_analyzer",
                 }
             })
         end
@@ -20,6 +22,13 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+            lspconfig.pylsp.setup({})
+            lspconfig.rust_analyzer.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
         end
     }
 }
