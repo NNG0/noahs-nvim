1fa12b3b173e2cc486ebe384cbda534a905afe0b
nng <noah.garkisch@gmail.com>
2025-08-29 11:17:30 +0000

    migrate to native lsp

---
+++ lua/config/lsp.lua
@@ -0,0 +1,23 @@
+vim.lsp.enable('lua_ls', 'python_ls')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client:supports_method('textDocument/completion') then
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+            vim.keymap.set('i', '<C-Space>', function()
+                vim.lsp.completion.get()
+            end)
+        end
+        if client:supports_method('textDocument/inlayHint') then
+            vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        end
+    end,
+})
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true
+    },
+})
